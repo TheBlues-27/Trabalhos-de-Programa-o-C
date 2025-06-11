5ba6f58742dec2ac3ebae1b9493cdcbe758a793b
Bruno Gabriel Caetano de Oliveira <155924027+TheBlues-27@users.noreply.github.com>
2025-06-11 15:11:51 +0000

    Adiciona variáveis para armazenar posições do maior e menor valor no array

--- Array 02.c
+++ Array 02.c
@@ -3,7 +3,7 @@
 
 int main(){
     int array[8];
-    int maior_valor = 0, menor_valor = 0;
+    int maior_valor = 0, menor_valor = 0, posicao_maior = 0, posicao_menor = 0;
     printf("Digite 8 numeros inteiros:\n");
     for (int i = 0; i < 8; i++) {
         scanf("%d", &array[i]);
@@ -13,12 +13,17 @@ int main(){
         } else {
             if (array[i] > maior_valor) {
                 maior_valor = array[i];
+                posicao_maior = i;
             }
             if (array[i] < menor_valor) {
                 menor_valor = array[i];
+                posicao_menor = i;
             }
         }
     }
     printf("O valor mais alto informado foi: %d\n", maior_valor);
+    printf("O valor mais alto informado esta na posicao: %d\n", posicao_maior);
     printf("O valor mais baixo informado foi: %d\n", menor_valor);
+    printf("O valor mais baixo informado esta na posicao: %d\n", posicao_menor);
+    return 0;
 }
